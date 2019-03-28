19689ef5a40faef4e6ff3a9facbad8626efff8de
cynic002 <xswerfdv@gmail.com>
2019-03-28 16:24:31 -0700

    init navigation

---
+++ Assets/navi/DestinationPlacement.cs
@@ -0,0 +1,20 @@
+﻿using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+
+public class DestinationPlacement : MonoBehaviour, IInputClickHandler
+{
+    void Start()
+    {
+        InputManager.Instance.PushFallbackInputHandler(gameObject);
+    }
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        if (GazeManager.Instance.IsGazingAtObject)
+        {
+            var hitInfo = GazeManager.Instance.HitInfo;
+            transform.position = hitInfo.point + transform.localScale.y * Vector3.up;
+            print(transform.position);
+        }
+    }
+}
---
+++ Assets/navi/HumanNavigation.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class HumanNavigation : MonoBehaviour
+{
+    public GameObject destination;
+    NavMeshAgent agent;
+    LineRenderer lineRenderer;
+
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        agent = GetComponent<NavMeshAgent>();
+        agent.updatePosition = false;
+    }
+
+    void Update()
+    {
+        agent.SetDestination(destination.transform.position); 
+        agent.nextPosition = Camera.main.transform.position;  
+
+        var positions = agent.path.corners;
+        lineRenderer.positionCount = positions.Length;
+        for (int i = 0; i < positions.Length; i++)
+        {
+            Debug.Log("point " + i + "=" + positions[i]);
+            lineRenderer.SetPosition(i, positions[i]);
+        }
+    }
+}
